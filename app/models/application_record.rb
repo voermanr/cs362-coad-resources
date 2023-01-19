1abbf49eb746486231da63add7eb95962f8bc1b1
Cooper Bailey <cjbailey02@gmail.com>
2023-01-19 10:49:19 -0800

    Added test to test rspec and added draft descriptions for each class in app/model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
+#ApplicationRecord inherits the methods and information from ActiveRecord class in Base
+#Represents the Record of the Application and sets the abstract_class variable to true
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+    self.abstract_class = true
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+  it "exists" do
+    org = Organization.new
+    expect(org).to be
+  end
 
 end

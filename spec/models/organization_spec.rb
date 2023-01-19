6ef7bc3acc4d880fdabcd30674cdbb3f3cf6e72a
Cooper Bailey <cjbailey02@gmail.com>
2023-01-19 11:36:59 -0800

    Created attribute tests for  organanization in organization_spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  it "exists" do
-    org = Organization.new
-    expect(org).to be
+  let (:org) {Organization.new}
+  it "responds to agreement attributes" do
+    expect(org).to respond_to(:agreement_one, :agreement_two, :agreement_three, :agreement_four, :agreement_five, :agreement_six, :agreement_seven, :agreement_eight)
   end
 
+  it "responds to presence_of attributes" do
+    expect(org).to respond_to(:email, :name, :phone, :status, :secondary_phone, :primary_name, :secondary_name)
+  end
+
+  it "responds to length_of attributes" do
+    expect(org).to respond_to(:description)
+  end
+
+  it "responds to after_initialize attributes" do
+  expect(org).to respond_to(:set_default_status)
+  end
+
+  it "responds to has_many attributes" do
+    expect(org).to respond_to(:users, :tickets)
+  end
 end

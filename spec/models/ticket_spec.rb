03b28417f462ec399af04120b27ef72f408d72df
Cooper Bailey <cjbailey02@gmail.com>
2023-01-22 14:06:02 -0800

    finished ticket_spec.rb file, added tests to check attributes

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  let (:ticket) {Ticket.new}
+
+  it "responds to belongs_to attributes" do
+    expect(ticket).to respond_to(:region, :resource_category, :organization)
+  end
+
+  it "responds to validates attributes" do
+    expect(ticket).to respond_to(:name, :phone, :region_id, :resource_category_id, :description)
+  end
+
 end

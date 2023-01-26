a142d5bef789a7e9ff8fc7eb7fca382c148d1f0c
Cooper Bailey <cjbailey02@gmail.com>
2023-01-26 11:20:34 -0800

    syncing

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe Organization, type: :model do
 
   describe 'validations' do
     it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:name)}
     it {should validate_length_of(:email)}
   end
 end

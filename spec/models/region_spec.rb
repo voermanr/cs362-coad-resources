aef0a935c3ca1e7c463d3aa60f516161797f9b57
Ross Voerman <voermanr@oregonstate.edu>
2023-01-26 11:13:12 -0800

    Fixed some whitespace

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-  let (:region) {Region.new}
+  let(:region) {Region.new}
   
   it "responds to attribute name" do
     expect(region).to respond_to(:name)

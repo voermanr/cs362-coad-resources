8699c8b24ef2f7994a2e9261e831778e549dab4c
Ross Voerman <voermanr@oregonstate.edu>
2023-01-28 12:05:36 -0800

    Implemented association tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let (:user) {User.new}
+  let(:user) {User.new}
 
   it "responds to attribute email" do
     expect(user).to respond_to(:email)
@@ -10,6 +10,11 @@ RSpec.describe User, type: :model do
 
   it "responds to attribute role" do
     expect(user).to respond_to(:role)
+  end
+
+
+  describe 'associations' do
+    it { should belong_to(:organization).optional }
   end
 
 end

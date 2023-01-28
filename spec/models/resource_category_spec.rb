6d24b2d37fac47e5826faacedd250851e8b7d791
Ross Voerman <voermanr@oregonstate.edu>
2023-01-28 13:45:36 -0800

    Implemented member function tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -18,8 +18,37 @@ RSpec.describe ResourceCategory, type: :model do
 
 
   describe 'associations' do
-     it { should have_and_belong_to_many(:organizations) }
-     it { should have_many(:tickets) }
+    it { should have_and_belong_to_many(:organizations) }
+    it { should have_many(:tickets) }
   end
 
+  describe '.inactive?' do
+    let(:res) { build(:resource_category, active: true) }
+
+    it { expect(res.inactive?).to be(false) }
+  end
+
+  describe '.activate' do
+    let(:res) { build(:resource_category, active: false) }
+
+    it 'calls .activate and expects active to be true' do
+      res.activate
+      expect(res.inactive?).to be(false)
+    end
+  end
+
+  describe '.deactivate' do
+    let(:res) { build(:resource_category, active: true) }
+
+    it 'calls .activate and expects active to be true' do
+      res.deactivate
+      expect(res.inactive?).to be(true)
+    end
+  end
+
+  describe '.to_s' do
+    let(:res) { build(:resource_category, name: 'Sheep') }
+
+    it { expect(res.to_s).to eq('Sheep') }
+  end
 end

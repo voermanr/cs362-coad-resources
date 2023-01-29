077a212084cc0b83f0897a5739141c1e651a4158
Cooper Bailey <cjbailey02@gmail.com>
2023-01-28 19:08:16 -0800

    finished validation tests for region_spec

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
   let(:region) {Region.new}
-  
+
   it "responds to attribute name" do
     expect(region).to respond_to(:name)
   end
@@ -15,6 +15,15 @@ RSpec.describe Region, type: :model do
     name = 'Mt. Hood'
     region = Region.new(name: name)
     result = region.to_s
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+
+    it {should validate_length_of(:name).is_at_least(1)}
+    it {should validate_length_of(:name).is_at_most(255)}
+
+    it {should validate_uniqueness_of(:name).ignoring_case_sensitivity }
   end
 
   describe 'associations' do

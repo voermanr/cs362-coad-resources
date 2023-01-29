195127f986893121418982dd3d682e5ee216d84f
Cooper Bailey <cjbailey02@gmail.com>
2023-01-28 18:59:03 -0800

    finished validation tests for organization_spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -38,9 +38,16 @@ RSpec.describe Organization, type: :model do
     it {should validate_presence_of(:secondary_name)}
     it {should validate_presence_of(:secondary_phone)}
 
+    it {should validate_length_of(:email).is_at_most(255)}
+    it {should validate_length_of(:email).is_at_least(1)}
+    it {should validate_length_of(:name).is_at_most(255)}
+    it {should validate_length_of(:email).is_at_least(1)}
+    it {should validate_length_of(:description).is_at_most(1020)}
+
     it {should allow_value("user@example.com").for(:email)}
 
     it {should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it {should validate_uniqueness_of(:name).ignoring_case_sensitivity }
   end
 
 

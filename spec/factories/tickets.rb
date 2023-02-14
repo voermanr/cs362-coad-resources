3777bd59c795e10883c4a9cf2ce5cc18dee222b1
Cooper Bailey <cjbailey02@gmail.com>
2023-02-14 11:26:23 -0800

    completed all scope tests, I feel amazing

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -3,7 +3,7 @@ gem 'faker'
 FactoryBot.define do
 
   factory :ticket do
-    name { Faker::TvShows::TwinPeaks.name }
+    name
     description {'fake ticket'}
     phone {'+1-555-555-5555'}
     region_id { create(:region).id }

8e5680b83fcbe2ef9097f74de52d8f32adaf10d8
Ross Voerman <voermanr@oregonstate.edu>
2023-02-18 14:55:22 -0800

    Finished quilting test blanket for regions_controller_spec.rb

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
-
   message_dashboard_redirect = 'expects to get redirected to the dashboard'
 
   context 'as a signed_in user' do
@@ -10,30 +9,66 @@ RSpec.describe RegionsController, type: :controller do
       sign_in(a_signed_in_user)
     end
 
+    # Tests start here
     describe 'GET #index' do
       it { expect(get(:index)).to redirect_to dashboard_path }
     end
 
     describe 'GET #show' do
-      it message_dashboard_redirect do
-        expect(get(:show, params: { id: attributes_for(:region) })).to redirect_to dashboard_path
-      end
+      it { expect(get(:show, params: { id: attributes_for(:region) })).to redirect_to dashboard_path }
     end
 
     describe 'GET #new' do
-      it do
-        expect(get(:new)).to redirect_to dashboard_path
-      end
+      it { expect(get(:new)).to redirect_to dashboard_path }
+    end
+
+    describe 'POST #create' do
+      it { expect(post(:create)).to redirect_to dashboard_path }
+    end
+
+    describe 'GET #edit' do
+      it { expect(get(:edit, params: { id: attributes_for(:region) })).to redirect_to dashboard_path }
+    end
+
+    describe 'PUT #update' do
+      it { expect(put(:update, params: { id: attributes_for(:region) })).to redirect_to dashboard_path }
+    end
+
+    describe 'DELETE #destroy' do
+      it { expect(delete(:destroy, params: { id: create(:region) })).to redirect_to dashboard_path }
     end
   end
 
   context 'as a logged out user' do
     let(:a_signed_out_user) { create(:user) }
 
+    # Tests start here
     describe 'GET #index' do
-      it message_dashboard_redirect do
-        expect(get(:index)).to redirect_to new_user_session_path
-      end
+      it { expect(get(:index)).to redirect_to new_user_session_path }
+    end
+
+    describe 'GET #show' do
+      it { expect(get(:show, params: { id: attributes_for(:region) })).to redirect_to new_user_session_path }
+    end
+
+    describe 'GET #new' do
+      it { expect(get(:new)).to redirect_to new_user_session_path }
+    end
+
+    describe 'POST #create' do
+      it { expect(post(:create)).to redirect_to new_user_session_path }
+    end
+
+    describe 'GET #edit' do
+      it { expect(get(:edit, params: { id: attributes_for(:region) })).to redirect_to new_user_session_path }
+    end
+
+    describe 'PUT #update' do
+      it { expect(put(:update, params: { id: attributes_for(:region) })).to redirect_to new_user_session_path }
+    end
+
+    describe 'DELETE #destroy' do
+      it { expect(delete(:destroy, params: { id: attributes_for(:region) })).to redirect_to new_user_session_path }
     end
   end
 
@@ -41,15 +76,33 @@ RSpec.describe RegionsController, type: :controller do
     let(:user) { create(:user, :admin) }
     before(:each) { sign_in(user) }
 
+    # Tests start here
     describe 'GET #index' do
       it { expect(get(:index)).to be_successful }
     end
 
-    describe 'GET #create' do
-      it do
-        post(:create, params: { region: attributes_for(:region) })
-        expect(response).to redirect_to regions_path
-      end
+    describe 'GET #show' do
+      it { expect(get(:show, params: { id: create(:region) })).to be_successful }
+    end
+
+    describe 'GET #new' do
+      it { expect(get(:new)).to be_successful }
+    end
+
+    describe 'POST #create' do
+      it { expect(post(:create, params: { region: attributes_for(:region) })).to redirect_to regions_path }
+    end
+
+    describe 'GET #edit' do
+      it { expect(get(:edit, params: { id: create(:region) })).to be_successful }
+    end
+
+    describe 'PUT #update' do
+      it { expect(put(:update, params: { id: create(:region), region: attributes_for(:region) })).to redirect_to region_path }
+    end
+
+    describe 'DELETE #destroy' do
+      it { expect(delete(:destroy, params: { id: create(:region) })).to redirect_to regions_path }
     end
   end
 end

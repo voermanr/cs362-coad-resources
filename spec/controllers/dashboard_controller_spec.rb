de48e2fe0ddd52020c74acdc94634db5ce5e1d08
Jay-JayG <jjgochez@gmail.com>
2023-03-07 11:27:35 -0800

    ticket controller render show

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-    context 'as a signed in user' do
-        let(:a_signed_in_user) { create(:user) }
-        before(:each) { sign_in(a_signed_in_user) }
-    
-        describe 'GET #index' do
-          it { expect(get(index)).to be_successful}
-        end
+  context 'as a signed in user' do
+    let(:a_signed_in_user) { create(:user) }
+    before(:each) { sign_in(a_signed_in_user) }
+  
+    describe 'GET #index' do
+      it { expect(get(index)).to be_successful}
+    end
+  end
 end

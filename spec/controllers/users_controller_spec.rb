5b0d7e9f716e78fce178232be4ef3a36db437098
Ross Voerman <voermanr@oregonstate.edu>
2023-03-11 10:20:55 -0800

    rubocop

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-   it { expect(UsersController.new).to be_truthy }
+  it { expect(UsersController.new).to be_truthy }
 
-   it { expect(Users::ConfirmationsController.new).to be }
+  it { expect(Users::ConfirmationsController.new).to be }
 
-   it { expect(Users::OmniauthCallbacksController.new).to be }
+  it { expect(Users::OmniauthCallbacksController.new).to be }
 
-   it { expect(Users::PasswordsController).to be }
+  it { expect(Users::PasswordsController).to be }
 
-   it { expect(Users::UnlocksController).to be }
+  it { expect(Users::UnlocksController).to be }
 end

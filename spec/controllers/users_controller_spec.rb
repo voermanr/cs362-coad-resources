26e013d468682fa8b6c6595ed2c02c9a13801446
Ross Voerman <voermanr@oregonstate.edu>
2023-03-11 10:18:55 -0800

    implemented Users::??? instantiation tests.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-   it {
-   user = UsersController.new 
-   expect(user).to be_truthy }
+   it { expect(UsersController.new).to be_truthy }
+
+   it { expect(Users::ConfirmationsController.new).to be }
+
+   it { expect(Users::OmniauthCallbacksController.new).to be }
+
+   it { expect(Users::PasswordsController).to be }
+
+   it { expect(Users::UnlocksController).to be }
 end

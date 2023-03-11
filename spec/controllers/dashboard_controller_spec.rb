d977f9d65290e4ca93142a49b70ce21dd0035719
Ross Voerman <voermanr@oregonstate.edu>
2023-03-11 10:10:22 -0800

    covered dashboard.rb with tests.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,12 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-    context 'as a signed in user' do
-        let(:a_signed_in_user) { create(:user) }
-        before(:each) { sign_in(a_signed_in_user) }
+  describe 'create a dashboard' do
+    it { expect(Dashboard).to be }
+  end
 
-        describe 'GET #index' do
-          it { expect(get(:index)).to be_successful}
-        end
+  context 'as a signed in user' do
+    let(:a_signed_in_user) { create(:user) }
+    before(:each) { sign_in(a_signed_in_user) }
+
+    describe 'GET #index' do
+      it { expect(get(:index)).to be_successful }
     end
+  end
 end

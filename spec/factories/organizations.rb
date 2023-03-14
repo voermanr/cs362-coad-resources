bbc2b0593c7f2e5ba6048aa64a33d07e68b264d6
Ross Voerman <voermanr@oregonstate.edu>
2023-03-14 15:22:27 -0700

    fixed organizations.rb FactoryBot

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,16 +1,20 @@
 FactoryBot.define do
   factory(:organization) do
-    name { Faker::TvShows::TwinPeaks.name}
+    name { Faker::Name.name}
     phone { Faker::PhoneNumber.cell_phone_with_country_code }
     email
     description { Faker::Lorem.words number: 4 }
     rejection_reason { Faker::Lorem.word }
     created_at { Faker::Date }
     updated_at { Faker::Date }
-    primary_name { Faker::TvShows::TwinPeaks.name }
-    secondary_name { Faker::TvShows::TwinPeaks.name }
+    primary_name { Faker::Name.name }
+    secondary_name { Faker::Name.name }
     secondary_phone { Faker::PhoneNumber.cell_phone_with_country_code }
     title {  }
     transportation {  }
+
+    trait :approved do
+      status { :approved }
+    end
   end
 end
